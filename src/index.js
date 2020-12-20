1d27ed22cfd8de703d9aef604baa8fe24eae452a
tejsubedi@gmail.com <tejsubedi@gmail.com>
2020-12-19 20:13:59 -0500

    keeper app working

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+
+
+ReactDOM.render(
+  <div>
+    <App/>
+  </div>,
+  document.getElementById('root')
+);
+
+
+
+//create a new React App
+//create a app.jsx component
+//create a header.jsx component that render the <header> element
+//to show the keeper app name in <h1></h1>
+//create a footer.jsx component that returns footer element
+//to show copyright message in a <p> with dynamically updated year
+//create a note.jsx to show div element with a h1 for title and p for content
+
+
+
+
